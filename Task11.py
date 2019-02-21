29579b188470202664c9386f891f8a2ad1045122
GuardGoose <justinszabo99@gmail.com>
2019-02-21 12:48:28 +0000

    I'm a mongo

--- Task11.py
+++ Task11.py
@@ -1,5 +1,5 @@
 """
-Delta(n) time for both adding n items to the list, and for querying the list.
+Theta(n) time for both adding n items to the list, and for querying the list.
 Implement your approach
 using the input and output described in Task 2.1.
 Call this file Task11.
@@ -8,19 +8,24 @@ Call this file Task11.
 import sys
 
 
-def search_list(start, stop):
-    temp = []
-    for i in range(int(start), int(stop)+1):
-        temp.append[i]
-    sys.stdout.write(str(temp))
+def search_list(list, start, stop):
+    # This function is Theta(n**2)
+    x = [y for y in list in range(int(start), int(stop) + 1)]
+    sys.stdout.write(str(x))
     sys.stdout.flush()
 
-if __name__ == '__main__':
+def main():
     a = []
     j = sys.stdin.readline().strip().split(" ")
     j1 = ''.join(j)
     for i in range(int(j1)):    
         for line in sys.stdin.readline().strip().split(" "):
             a.append(line)
+    print(a)    # DEBUG
     start, stop = sys.stdin.readline().strip().split(" ")
-    search_list(start, stop)
+    search_list(a, start, stop)
+
+
+
+if __name__ == '__main__':
+    main()
